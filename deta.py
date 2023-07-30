0325b9f392c13c2ebbb18f0724d00e28d55ef25d
spykelionel <ndilionel6@gmail.com>
2023-07-30 23:48:30 +0100

    init files

---
+++ deta.py
@@ -0,0 +1,13 @@
+def evaluate_model(y_true, y_pred, model_name):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    roc_auc = roc_auc_score(y_true, y_pred)
+    print(f"{model_name} Performance:")
+    print(f"Accuracy: {accuracy:.2f}")
+    print(f"Precision: {precision:.2f}")
+    print(f"Recall: {recall:.2f}")
+    print(f"F1 Score: {f1:.2f}")
+    print(f"AUC-ROC Score: {roc_auc:.2f}")
+    print()
